164d26a3e4ba6a2651a2ad3f7c10f78ac8e54cb0
Nick Cruz <nickcruz920@gmail.com>
2022-07-24 07:58:51 -0400

    Generate Btn Function

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ let generateHeading = () => {
   generateHeading.innerHTML = `<h2 class="text-primary text-center pt-2 pb-2 fw-bold fs-4">Heading</h2>`;
   
 }
+
+  let generateBtn = () => {
+    let generateBtn = document.getElementById("generateBtn");
+    
+    generateBtn.innerHTML = `<div class="btn btn-success text-light border-dark w-50 d-block m-auto">Button</button>`;
+    
+  }
