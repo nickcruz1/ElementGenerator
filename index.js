b349130e4d4cace516aa75b6ab9e8060ae15423f
Nick Cruz <nickcruz920@gmail.com>
2022-07-24 07:58:03 -0400

    Generate Text Function Added

---
+++ index.js
@@ -0,0 +1,8 @@
+let generateText = () => {
+  let generateText = document.querySelector("#generateText");
+  
+  generateText.innerHTML = `
+  <div class="text-dark pt-2 pb-2 text-center">Text</div>
+  `;
+  
+}
