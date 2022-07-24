2d949f7cb2bfad98a4e02029a513a446255e9b48
Nick Cruz <nickcruz920@gmail.com>
2022-07-24 07:59:36 -0400

    Generate List Function

--- index.js
+++ index.js
@@ -27,3 +27,15 @@ let generateHeading = () => {
     generateParagraph.innerHTML = `<p class="text-center pt-2 pb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolorem dicta atque voluptate ea explicabo libero eligendi commodi sed sit pariatur illum beatae vel modi quaerat, in nemo laudantium incidunt?</p>`
     
   }
+    
+      let generateList = () => {
+    let generateList = document.querySelector("#generateList");
+    
+    generateList.innerHTML = `<ul class="text-center pt-2 pb-2">
+    <li>List Item 1
+<li>List Item 2
+<li>List Item 3
+</ul>
+    `;
+    
+  }
